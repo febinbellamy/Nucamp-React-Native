be4a7fcc723e2090d2ec8ac955fabc743392a9bc
Febin Bellamy <febinbellamy@gmail.com>
2022-03-13 10:49:25 -0400

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -8,7 +8,7 @@ import {
   Button,
   StyleSheet,
   Alert,
-  PanResponder
+  PanResponder, Share
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 
@@ -34,13 +34,12 @@ const mapDispatchToProps = {
 
 function RenderCampsite(props) {
 
-  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
-
   const { campsite } = props;
 
   const view = React.createRef();
 
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -81,6 +80,17 @@ function RenderCampsite(props) {
     },
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share({
+      title, 
+      message: `${title}: ${message} ${url}`,
+      url
+    }, {
+      dialogTitle: 'Share ' + title
+    });
+  };
+
+
   if (campsite) {
     return (
       <Animatable.View
@@ -115,6 +125,14 @@ function RenderCampsite(props) {
               raised
               reverse
               onPress={() => props.onShowModal()}
+            />
+            <Icon
+              name='share'
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
             />
           </View>
         </Card>
